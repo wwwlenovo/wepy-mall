7d5b7925e750f9ed3320de6b6317224592681298
Guo Chen <guo.chen@sap.com>
2019-06-18 17:47:10 +0800

    add categories collection

--- cloudfunctions/getHomeDiscoverList/index.js
+++ cloudfunctions/getHomeDiscoverList/index.js
@@ -2,19 +2,18 @@
 const cloud = require('wx-server-sdk')
 cloud.init()
 const db = cloud.database()
-const productsCollection = db.collection('Products')
+const CategoriesCollection = db.collection('Categories')
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const count = await productsCollection.where({
-    InDiscoverList:true
-  }).count();
+  const count = await CategoriesCollection.where({}).count();
   const page = event.page;
   const size = event.size;
-  const res = await productsCollection.where({InDiscoverList:true}).skip((page-1)*size).limit(size).field({
-    Products_Name:true,
-    logo:true,
-    TargetUrl:true
+  const res = await CategoriesCollection.where({}).skip((page-1)*size).limit(size).field({
+    Category_Code:true,
+    Category_Name:true,
+    Category_Logo:true,
+    Target_Url:true
   }).get();
   res['total'] = count.total;
   return res;
