0e3e98cedc6b81961b1dce228d50b3d2e583edeb
Guo Chen <guo.chen@sap.com>
2019-06-18 16:43:51 +0800

    update

--- cloudfunctions/getHomeDiscoverList/index.js
+++ cloudfunctions/getHomeDiscoverList/index.js
@@ -7,13 +7,14 @@ const productsCollection = db.collection('Products')
 // 云函数入口函数
 exports.main = async (event, context) => {
   const count = await productsCollection.where({
-    inDiscoverList:true
+    InDiscoverList:true
   }).count();
   const page = event.page;
   const size = event.size;
-  const res = await productsCollection.where({inDiscoverList:true}).skip((page-1)*size).limit(size).field({
+  const res = await productsCollection.where({InDiscoverList:true}).skip((page-1)*size).limit(size).field({
     Products_Name:true,
-    logo:true
+    logo:true,
+    TargetUrl:true
   }).get();
   res['total'] = count.total;
   return res;
