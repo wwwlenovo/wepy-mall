e476de5f146d6a3ffb14170b411b26b1464bf797
Guo Chen <guo.chen@sap.com>
2019-07-02 23:32:12 +0800

    save

---
+++ cloudfunctions/addToCart/index.js
@@ -0,0 +1,34 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+cloud.init()
+const db = cloud.database()
+const cartCollection = db.collection('Cart')
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const goods = await cartCollection.where({
+      openId:event.openId,
+      goodsId:event.goodsId,
+      skuVal:event.skuVal
+  }).get();
+  if(goods.data.length === 0){
+     return await cartCollection.add({
+       data:{
+        openId:event.openId,
+        goodsId:event.goodsId,
+        skuVal:event.skuVal,
+        orderNum: event.orderNum
+        }
+      });
+  }else {
+      return await cartCollection.where({
+        openId:event.openId,
+        goodsId:event.goodsId,
+        skuVal:event.skuVal
+    }).update({
+        data:{
+         orderNum: event.orderNum
+         }
+       });
+  }
+}
