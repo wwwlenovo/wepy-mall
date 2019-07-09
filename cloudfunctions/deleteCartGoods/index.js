d421baed0ef027caa44403a3392442d510bce258
Guo Chen <guo.chen@sap.com>
2019-07-09 16:55:25 +0800

    remove cart

--- cloudfunctions/deleteCartGoods/index.js
+++ cloudfunctions/deleteCartGoods/index.js
@@ -2,12 +2,13 @@
 const cloud = require('wx-server-sdk')
 cloud.init()
 const db = cloud.database()
+const _ = db.command;
 const cartCollection = db.collection('Cart')
 
 
 exports.main = async (event, context) => {
     return await cartCollection.where({
         openId:event.openId,
-        _id:event.id
+        _id:_.in(event.ids)
     }).remove();
 }
